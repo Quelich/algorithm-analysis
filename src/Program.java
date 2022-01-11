800ba3d3139610d90bf62cebe16264331863bbd1
Quelich <emre1kilic@outlook.com>
2022-01-11 22:59:51 +0300

    quicksort error

--- src/Program.java
+++ src/Program.java
@@ -1,14 +1,12 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
 
 public class Program {
     static long quickSortElapsedTime;
 
-    public static void printInsertionSort(int[] data) {
+    public static void printInsertionSort(double[] data) {
         long startTime = System.currentTimeMillis();
         for (int i = 1; i < data.length; i++) {
-            int newElement = data[i];
+            double newElement = data[i];
             int j;
             for (j = i; j > 0 && data[j - 1] > newElement; j--) {
                 data[j] = data[j - 1];
@@ -19,10 +17,10 @@ public class Program {
         long endTime = System.currentTimeMillis();
         long elapsed = endTime - startTime;
         //print result
-        System.out.println("Insertion Sort: Array Size: " + data.length + " Time Elapsed: " + elapsed);
+        System.out.println("Insertion Sort: Array Size: " + data.length + " Time Elapsed: " + elapsed + " ms");
     }
 
-    public static void printSelectionSort(int[] data) {
+    public static void printSelectionSort(double[] data) {
         long startTime = System.currentTimeMillis();
         for (int i = data.length - 1; i > 0; i--) {
             int largest = 0;
@@ -37,33 +35,33 @@ public class Program {
         long endTime = System.currentTimeMillis();
         long elapsed = endTime - startTime;
         //print result
-        System.out.println("Selection Sort: Array Size: " + data.length + " Time Elapsed: " + elapsed);
+        System.out.println("Selection Sort: Array Size: " + data.length + " Time Elapsed: " + elapsed+ " ms");
     }
 
-    public static void printQuickSort(int[] data) {
+    public static void printQuickSort(double[] data) {
         long startTime = System.currentTimeMillis();
         quickSort(data, 0, data.length);
         //print result
         long endTime = System.currentTimeMillis();
         quickSortElapsedTime = endTime - startTime;
-        System.out.println("Quick Sort: Array Size: " + data.length + " Time Elapsed: " + quickSortElapsedTime);
+        System.out.println("Quick Sort: Array Size: " + data.length + " Time Elapsed: " + quickSortElapsedTime+ " ms");
     }
 
 
-    public static void printMergeSort(int[] data) {
+    public static void printMergeSort(double[] data) {
 
         long startTime = System.currentTimeMillis();
         mergeSort(data, 0, data.length);
         long endTime = System.currentTimeMillis();
         long elapsed = endTime - startTime;
-        System.out.println("Merge Sort: Array Size: " + data.length + " Time Elapsed: " + elapsed);
+        System.out.println("Merge Sort: Array Size: " + data.length + " Time Elapsed: " + elapsed+ " ms");
     }
 
-    public static void printShellSort(int[] data) {
+    public static void printShellSort(double[] data) {
         long startTime = System.currentTimeMillis();
         for (int gap = data.length / 2; gap > 0; gap /= 2) {
             for (int i = gap; i < data.length; i++) {
-                int newElement = data[i];
+                double newElement = data[i];
 
                 int j = i;
 
@@ -78,20 +76,20 @@ public class Program {
         long endTime = System.currentTimeMillis();
         long elapsed = endTime - startTime;
         //print result
-        System.out.println("Shell Sort: Array Size: " + data.length + " Time Elapsed: " + elapsed);
+        System.out.println("Shell Sort: Array Size: " + data.length + " Time Elapsed: " + elapsed+ " ms");
     }
 
-    public static void swap(int[] data, int i, int j) {
+    public static void swap(double[] data, int i, int j) {
         if (i == j) return;
-        int temp = data[i];
+        double temp = data[i];
         data[i] = data[j];
         data[j] = temp;
     }
 
     /* Helper methods */
-    public static int partition(int[] data, int start, int end) {
+    public static int partition(double[] data, int start, int end) {
         // Starting element is the pivot
-        int pivot = data[start];
+        double pivot = data[start];
         int i = start;
         int j = end;
 
@@ -111,7 +109,7 @@ public class Program {
 
     }
 
-    public static void mergeSort(int[] data, int start, int end) {
+    public static void mergeSort(double[] data, int start, int end) {
 
         if (end - start < 2) return;
         int mid = (start + end) / 2;
@@ -120,13 +118,13 @@ public class Program {
         merge(data, start, mid, end);
     }
 
-    public static void merge(int[] data, int start, int mid, int end) {
+    public static void merge(double[] data, int start, int mid, int end) {
 
         if (data[mid - 1] <= data[mid])  return;
         int i = start;
         int j = mid;
         int tempIndex = 0;
-        int[] temp = new int[end - start];
+        double[] temp = new double[end - start];
         while (i < mid && j < end) {
             temp[tempIndex++] = data[i] <= data[j] ? data[i++] : data[j++];
         }
@@ -134,7 +132,7 @@ public class Program {
         System.arraycopy(temp, 0, data, start, tempIndex);
     }
 
-    private static void quickSort(int[] data, int start, int end) {
+    private static void quickSort(double[] data, int start, int end) {
 
         if (end - start < 2) return;
         int pivotIndex = partition(data, start, end);
@@ -142,24 +140,51 @@ public class Program {
         quickSort(data, pivotIndex + 1, end);
     }
 
-    public static void getArray() throws FileNotFoundException {
+    public static double[] getArray() throws IOException {
+        double[] data = new double[300000];
       File file = new File("Query.txt");
-      Scanner sc = new Scanner(file);
-      while(sc.hasNextLine())
-      {
-        System.out.println(sc.nextLine());
-      }
+        BufferedReader br
+                = new BufferedReader(new FileReader(file));
+
+        // Declaring a string variable
+        String line;
+        // Condition holds true till
+        // there is character in a string
+        int i = 0;
+        while ((line = br.readLine()) != null)
+        {
+            double number = convertNumber(line);
+            data[i] = number;
+            i++;
+        }
+        System.out.println("Counter: " + i);
+        return data;
     }
 
-    private static int convertNumber(String sqc)
+    private static double convertNumber(String line)
     {
-
-        return -1;
+        StringBuilder seq1 = new StringBuilder();
+        // get the sequence before the ten power
+        for(int i = 0; i<line.length(); i++)
+        {
+            if(line.charAt(i) == 'e')
+                break;
+            seq1.append(line.charAt(i));
+        }
+        char powerVal = line.charAt(line.length() - 1);
+        double tenPower =  Integer.parseInt(String.valueOf(powerVal));
+        double num = Double.parseDouble(seq1.toString());
+        return num / Math.pow(10,tenPower);
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
        
-        getArray();
+        double[] data = getArray();
+        printInsertionSort(data);
+        printSelectionSort(data);
+        printMergeSort(data);
+        printShellSort(data);
+        printQuickSort(data);
     }
 }
 
